b11c3ac9ce70ba0f07db500bf2488aa39b7c7c66
Brunix2018 <42790138+Brunix2018@users.noreply.github.com>
2023-04-24 21:49:55 -0300

    metodo para que un enemigo indique si se va a mover o no

--- tpia/src/enemigos/PokeEnemigo.java
+++ tpia/src/enemigos/PokeEnemigo.java
@@ -2,8 +2,10 @@ package enemigos;
 
 public class PokeEnemigo {
     private final Integer energia;
+    private Integer cooldownMoverse;
     public PokeEnemigo(Integer energia) {
         this.energia=energia;
+        this.cooldownMoverse = (int) ((Math.random() * ((3 - 1) + 1)) + 3);
     }
 
     public Integer getEnergia() {
@@ -12,5 +14,14 @@ public class PokeEnemigo {
 
     public String toString(){
         return this.energia.toString();
+    }
+
+    public boolean moverse(){
+        this.cooldownMoverse-=1;
+        if (cooldownMoverse == 0){
+         this.cooldownMoverse = (int) ((Math.random() * ((3 - 1) + 1)) + 3);
+         return true;
+        }
+        else return false;
     }
 }
